fd65967bfb6fffbbb96a031faa66ec977f495a46
ugur <iyigunlerugur@gmail.com>
2023-07-03 19:24:26 +0300

    son java

---
+++ src/main/java/day33exceptionsenumsiterator/InvalidNumberException.java
@@ -0,0 +1,10 @@
+package day33exceptionsenumsiterator;
+
+public class InvalidNumberException extends RuntimeException{
+
+
+
+        public InvalidNumberException(String message){
+            super(message);
+        }
+}
---
+++ src/main/java/day33exceptionsenumsiterator/Iterators01.java
@@ -0,0 +1,80 @@
+package day33exceptionsenumsiterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Iterators01 {
+    public static void main(String[] args) {
+
+ /*
+        1) Iterator' lar looplarin yaptigi ayni isi yapar
+        2) Iterator'larda sonsus loop olusma ihtimali yoktur
+        3) Iterator'lar ile loop'lar arasinda performans farki yoktur.
+        4) Iterator'lar bir collection'dan eleman silme ve bir collection daki elemani degistirme konusunda daha basarilidir
+        5) Iki tip iterator vardir;
+            a) Iterator : Bu sadece eleman silmede kullanilir
+                          Eleman eklemek veya elemani degistirmek mumkun degildir
+
+            b) ListIterator: Bu eleman silebilir, ekleyebilir ve degistirebilir.
+
+            Note: "Iterator" sadece soldan saga (ilk elemandan son elemana) calisir
+                   "ListIterator" ise iki yonlu calisabilir
+         */
+
+
+
+   List<String> myList=new ArrayList<>();
+   myList.add("Emin");
+   myList.add("Bugra");
+   myList.add("Hasan");
+   myList.add("Benna");
+   myList.add("Ali");
+        System.out.println(myList);
+
+Iterator<String>myItr=myList.iterator();// [Emin, Bugra, Hasan, Benna, Ali]
+
+while(myItr.hasNext()){
+    myItr.next();
+    myItr.remove();
+}
+        System.out.println(myList);
+  //ListIterator kullanimi
+
+        List<String>yourList=new ArrayList<>();
+        yourList.add("Hasan");
+        yourList.add("Benna");
+        yourList.add("Ali");
+        System.out.println();
+        ListIterator<String>yourListItr=yourList.listIterator();
+        while(yourListItr.hasNext()){
+            String el=yourListItr.next();
+            yourListItr.set(el+ "*");
+
+        }
+        System.out.println(yourList);
+
+    //previous method kullanimi
+        List<String>list3=new ArrayList<>();
+        list3.add("Hasan");
+        list3.add("Benna");
+        list3.add("Ali");
+        System.out.println(list3);//[Hasan, Benna, Ali]
+
+        ListIterator<String> listItr=list3.listIterator();
+
+        while(listItr.hasNext()){
+            listItr.next();
+        }
+        //pointer i  yukaridaki kol ile en sonda biraktigimiz icin
+        // //elemanlari en sondan basa dogru  yazdirabiliriz
+
+        while(listItr.hasPrevious()){
+        String el= listItr.previous();
+            System.out.println(el+ "<===");
+        }
+
+    }
+
+}
